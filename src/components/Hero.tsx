e6570caee566202a6645810a455f3b899ac28316
Samuel Hepworth <samhep0803@gmail.com>
2022-04-29 16:05:11 +0400

    Finish initial part of hero (can be finished at a later date)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ interface HeroProps {}
 export const Hero: React.FC<HeroProps> = ({}) => {
   return (
     <>
-      <Container maxW={"9xl"}>
+      <Container maxW={"6xl"}>
         <Stack
           as={Box}
           textAlign={"center"}
@@ -25,20 +25,15 @@ export const Hero: React.FC<HeroProps> = ({}) => {
         >
           <Heading
             fontWeight={600}
-            fontSize={{ base: "3xl", sm: "4xl", md: "9xl" }}
+            fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
             lineHeight={"110%"}
           >
             <Flex alignSelf={"center"} justifyContent="center">
-              <Text as="span" alignSelf="center">
-                CROSS THE
-              </Text>
-              <Img w="20%" h="20%" src="/Land.png" alignSelf="center" pl={10} />
+              <Img src="/Land.png" alignSelf="center" />
             </Flex>
           </Heading>
-          <Text color={"gray.500"} maxW="3xl">
-            Monetize your content by charging your most loyal readers and reward
-            them loyalty points. Give back to your loyal readers by granting
-            them access to your pre-releases and sneak-peaks.
+          <Text color={"gray.500"} maxW="2xl" fontSize="xl">
+            Welcome to VATMENA and VATEUD's biggest bi-annual event of the year!
           </Text>
           <Stack
             direction={"column"}
@@ -49,14 +44,14 @@ export const Hero: React.FC<HeroProps> = ({}) => {
           >
             <Button
               colorScheme={"green"}
-              bg={"green.400"}
+              bg={"primary.400"}
               rounded={"full"}
               px={6}
               _hover={{
-                bg: "green.500",
+                bg: "primary.500",
               }}
             >
-              Get Started
+              Login
             </Button>
             <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
               Learn more
